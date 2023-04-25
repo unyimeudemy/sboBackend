c8b1c9d694bf672ee65378e9958d47d222356526
unyimeudoh <unyimeudemy20@gmail.com>
2023-04-25 02:56:22 -0700

    test

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const connect = () => {
 
 // app.use(express.static("/public/index.html")); // relative path of client-side code
 app.get("/*", (req, res) => {
-  res.sendFile(path.join("/public/index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use(compression());
