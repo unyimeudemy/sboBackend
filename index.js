2bfb7b5461c24fa99036e68c0cfd10b944dc4e83
unyimeudoh <unyimeudemy20@gmail.com>
2023-04-24 17:21:51 -0700

    m

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ const connect = () => {
     });
 };
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public", "index.html"));
+// });
+
 app.use(compression());
 app.use("/api/user", userRoutes);
 app.use("/api/sbo", sboRoutes);
